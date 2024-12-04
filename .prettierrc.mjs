83fd932f2085da2d4fb81cd3a787c506934bd023
webdev03 <75148774+webdev03@users.noreply.github.com>
2024-12-04 20:59:53 +1300

    Revert "switch to giscus component in repo"
    This reverts commit dda94e124a6423b8ea57320b96b04b7565906d1b. I will try to fix this later.

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,17 +1,11 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-svelte"],
+  plugins: ["prettier-plugin-astro"],
   overrides: [
     {
       files: "*.astro",
       options: {
         parser: "astro"
-      }
-    },
-    {
-      files: "*.svelte",
-      options: {
-        parser: "svelte"
       }
     }
   ],
--- astro.config.mts
+++ astro.config.mts
@@ -2,13 +2,11 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
-import svelte from "@astrojs/svelte";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://devarsh.me",
   output: "static",
-  integrations: [svelte()],
+  integrations: [],
   adapter: cloudflare({
     imageService: "cloudflare"
   })
